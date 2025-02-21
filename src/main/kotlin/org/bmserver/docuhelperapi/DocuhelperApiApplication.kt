cdc2b3fa5025c2c2aa2dc68493d700a27a6ff253
BM <jmspon33@gmail.com>
2025-02-21 15:41:11 +0900

    프로젝트 생성

---
+++ src/main/kotlin/org/bmserver/docuhelperapi/DocuhelperApiApplication.kt
@@ -0,0 +1,11 @@
+package org.bmserver.docuhelperapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DocuhelperApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DocuhelperApiApplication>(*args)
+}
---
+++ src/test/kotlin/org/bmserver/docuhelperapi/DocuhelperApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bmserver.docuhelperapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DocuhelperApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
