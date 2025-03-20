87104b2fd2c938eebca07f4c7ae8ba204b719b3f
BmSrv <jmspon33@gmail.com>
2025-03-20 22:49:21 +0900

    DefaultLogin 추가

--- gateway/src/main/kotlin/org/bmserver/gateway/config/gql/CustomContextFactory.kt
+++ gateway/src/main/kotlin/org/bmserver/gateway/config/gql/CustomContextFactory.kt
@@ -6,6 +6,7 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import io.jsonwebtoken.JwtException
 import org.bmserver.gateway.config.security.JwtUtil
 import org.bmserver.gateway.config.security.User
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import java.util.UUID
@@ -16,10 +17,23 @@ private val logger = KotlinLogging.logger { }
 class CustomContextFactory(
     private val jwtUtil: JwtUtil,
 ) : DefaultSpringGraphQLContextFactory() {
+
+    @Value("\${dev.default.login.enable}")
+    private lateinit var testLoginEnable: String
+
+    @Value("\${dev.default.login.user.uuid}")
+    private lateinit var testLoginUUID: String
+
+    @Value("\${dev.default.login.user.email}")
+    private lateinit var testLoginEmail: String
+
+
     override suspend fun generateContext(request: ServerRequest): GraphQLContext {
         val jwtToken = request.cookies()["JWT_TOKEN"]?.get(0)?.value
 
-        val user =
+        val user = if (testLoginEnable.toBoolean()) {
+            User(UUID.fromString(testLoginUUID), testLoginEmail)
+        } else {
             try {
                 jwtToken?.let {
                     val userClaims = jwtUtil.parseJwt(it)
@@ -32,6 +46,7 @@ class CustomContextFactory(
                 logger.error { e }
                 null
             }
+        }
 
         val context =
             GraphQLContext
