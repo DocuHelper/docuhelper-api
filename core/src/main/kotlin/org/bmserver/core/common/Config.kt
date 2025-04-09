3dd220e5bbefadf3378655714c4cd44da328867d
BmSrv <jmspon33@gmail.com>
2025-04-09 17:52:39 +0900

    배포 ConfigMap 적용 및 포트 변경

--- core/src/main/kotlin/org/bmserver/core/common/Config.kt
+++ core/src/main/kotlin/org/bmserver/core/common/Config.kt
@@ -4,6 +4,7 @@ import java.util.UUID
 
 class Config {
     companion object {
-        val serverUuid: UUID = UUID.fromString("dd9a4830-3bac-4763-8634-0e5bebab2c9c")
+        val serverUuid: UUID = UUID.randomUUID()
+//        val serverUuid: UUID = UUID.fromString("dd9a4830-3bac-4763-8634-0e5bebab2c9c")
     }
 }
