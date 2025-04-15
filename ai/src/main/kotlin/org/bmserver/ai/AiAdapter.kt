79d32526933d1d957da81db1b7c58232de1b31ce
BmSrv <jmspon33@gmail.com>
2025-04-15 14:55:46 +0900

    Ai 모듈 구성 (채팅 추가

--- ai/src/main/kotlin/org/bmserver/ai/AiAdapter.kt
+++ ai/src/main/kotlin/org/bmserver/ai/AiAdapter.kt
@@ -3,14 +3,20 @@ package org.bmserver.ai
 import org.bmserver.ai.ollama.OllamaEmbeddingRequest
 import org.bmserver.ai.ollama.OllamaEmbeddingResponse
 import org.bmserver.core.ai.AiOutPort
+import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.model.openai.autoconfigure.OpenAiChatProperties
+import org.springframework.ai.openai.OpenAiChatModel
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Component
 class AiAdapter(
-    private val ollamaClient: WebClient
+    private val ollamaClient: WebClient,
+    private val openAiChatModel: OpenAiChatModel,
+    private val openAiChatProperties: OpenAiChatProperties,
 ) : AiOutPort {
     @Value("\${spring.ai.ollama.embedding.model}")
     lateinit var embeddingModel: String
@@ -27,5 +33,12 @@ class AiAdapter(
             .retrieve()
             .bodyToMono(OllamaEmbeddingResponse::class.java)
             .map { it.embedding }
+    }
+
+    override fun getAnswer(text: String): Flux<String> {
+        val prompt = Prompt(text, openAiChatProperties.options)
+
+        return openAiChatModel.internalStream(prompt, null)
+            .map { it.result.output.text ?: "" }
     }
 }
--- core/src/main/kotlin/org/bmserver/core/ai/AiOutPort.kt
+++ core/src/main/kotlin/org/bmserver/core/ai/AiOutPort.kt
@@ -1,7 +1,10 @@
 package org.bmserver.core.ai
 
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface AiOutPort {
     fun getEmbedding(text: String): Mono<List<Float>>
+
+    fun getAnswer(text: String): Flux<String>
 }
